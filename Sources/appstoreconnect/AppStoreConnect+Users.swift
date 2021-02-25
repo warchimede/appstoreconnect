0334b167c0cb2fa8f146fa4627891af83d1b8c20
William Archimede <william.archimede@protonmail.com>
2021-02-25 16:33:30 +0100

    Refactor and slice up the common setup

--- Sources/appstoreconnect/AppStoreConnect+Users.swift
+++ Sources/appstoreconnect/AppStoreConnect+Users.swift
@@ -8,28 +8,12 @@ extension AppStoreConnect {
     @OptionGroup var options: AppStoreConnect.Options
 
     mutating func run() throws {
-      guard let key = AppStoreConnect.key(for: options.keyId)
-      else {
-        print("ERROR Key data")
-        return
+      AppStoreConnect.fetch(endpoint: .users, options: options) { result in
+        switch result {
+        case .failure(let error): print("ERROR: \(error)")
+        case .success(let data): print(String(data: data, encoding: .utf8) ?? "Error: \(AppStoreConnectError.undecodableData)")
+        }
       }
-
-      let request = AppStoreConnect.request(endpoint: .users, key: key, keyId: options.keyId, issId: options.issId)
-
-      AppStoreConnect.dispatchGroup?.enter()
-      URLSession.shared.dataTask(with: request, completionHandler: { data, _, error in
-        if let error = error {
-          print("ERROR RESPONSE \(error)")
-          return
-        }
-
-        if let result = data.flatMap({ String(data: $0, encoding: .utf8) }) {
-          print(result)
-        } else {
-          print("ERROR NO DATA")
-        }
-        AppStoreConnect.dispatchGroup?.leave()
-      }).resume()
     }
   }
 }
--- Sources/appstoreconnect/AppStoreConnect.swift
+++ Sources/appstoreconnect/AppStoreConnect.swift
@@ -3,6 +3,14 @@ import Foundation
 import SwiftJWT
 
 struct AppStoreConnect: ParsableCommand {
+  enum AppStoreConnectError: Error {
+    case invalidResponse(URLResponse?)
+    case keyNotFound
+    case invalidKeyData
+    case noDataReceived
+    case undecodableData
+  }
+
   static var configuration = CommandConfiguration(
     commandName: "appstoreconnect",
     abstract: "An interface App Store Connect API",
@@ -11,23 +19,59 @@ struct AppStoreConnect: ParsableCommand {
 
   static var dispatchGroup: DispatchGroup?
 
-  static func key(for keyId: String) -> Data? {
+  private static func key(for keyId: String) -> Result<Data, AppStoreConnectError> {
     let keyFilePath = "\(FileManager.default.homeDirectoryForCurrentUser.path)/.appstoreconnect/private_keys/AuthKey_\(keyId.uppercased()).p8"
-    guard FileManager.default.fileExists(atPath: keyFilePath) else { return nil }
+    guard FileManager.default.fileExists(atPath: keyFilePath)
+    else { return .failure(.keyNotFound) }
 
-    return FileManager.default.contents(atPath: keyFilePath)
+    guard let keyData = FileManager.default.contents(atPath: keyFilePath)
+    else { return .failure(.invalidKeyData) }
+
+    return .success(keyData)
   }
 
-  static func request(endpoint: Endpoint, key: Data, keyId: String, issId: String) -> URLRequest {
-    let header = Header(typ: "JWT", kid: keyId)
-      let signer = JWTSigner.es256(privateKey: key)
-      let claims = APIClaims(exp: Date(timeIntervalSinceNow: 3600), iss: issId)
-      var jwt = JWT(header: header, claims: claims)
-      let signedJWT = (try? jwt.sign(using: signer)) ?? ""
+  private static func createRequest(endpoint: Endpoint, key: Data, options: Options) -> URLRequest {
+    let header = Header(typ: "JWT", kid: options.keyId)
+    let signer = JWTSigner.es256(privateKey: key)
+    let claims = APIClaims(exp: Date(timeIntervalSinceNow: 3600), iss: options.issId)
+    var jwt = JWT(header: header, claims: claims)
+    let signedJWT = (try? jwt.sign(using: signer)) ?? ""
 
-      var request = URLRequest(url: endpoint.url)
-      request.addValue("Bearer \(signedJWT)", forHTTPHeaderField: "Authorization")
+    var request = URLRequest(url: endpoint.url)
+    request.addValue("Bearer \(signedJWT)", forHTTPHeaderField: "Authorization")
 
-      return request
+    return request
+  }
+
+  private static func sendRequest(_ request: URLRequest, completion: @escaping (Result<Data, Error>) -> Void) {
+    AppStoreConnect.dispatchGroup?.enter()
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+      defer {
+        AppStoreConnect.dispatchGroup?.leave()
+      }
+
+      let statusCode = (response as? HTTPURLResponse)?.statusCode
+      guard statusCode == 200 else {
+        completion(.failure(error ?? AppStoreConnectError.invalidResponse(response)))
+        return
+      }
+
+      guard let data = data else {
+        completion(.failure(error ?? AppStoreConnectError.noDataReceived))
+        return
+      }
+
+      completion(.success(data))
+    }
+    task.resume()
+  }
+
+  static func fetch(endpoint: Endpoint, options: Options, completion: @escaping (Result<Data, Error>) -> Void) {
+    switch key(for: options.keyId) {
+    case .failure(let error): completion(.failure(error))
+    case .success(let key):
+      let request = createRequest(endpoint: endpoint, key: key, options: options)
+      sendRequest(request, completion: completion)
+    }
   }
 }
