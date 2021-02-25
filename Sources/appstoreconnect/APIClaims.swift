affc27705739341d9557c1627a9695321150f94c
William Archimede <william.archimede@protonmail.com>
2021-02-25 18:52:40 +0100

    Update APIClaims

--- Sources/appstoreconnect/APIClaims.swift
+++ Sources/appstoreconnect/APIClaims.swift
@@ -2,8 +2,17 @@ import Foundation
 import SwiftJWT
 
 struct APIClaims: Claims {
-  let admin = true
-  let aud = "appstoreconnect-v1"
+  let aud: String
   let exp: Date
+  let isAdmin: Bool
   let iss: String
+  let sub: String
+
+  init(aud: String = "appstoreconnect-v1", exp: Date, isAdmin: Bool = true, iss: String, sub: String = "jenkins") {
+    self.aud = aud
+    self.exp = exp
+    self.isAdmin = isAdmin
+    self.iss = iss
+    self.sub = sub
+  }
 }
