80cf2abe8e3c7ab1f5a77bf325c856752f15469b
William Archimede <william.archimede@protonmail.com>
2021-02-25 12:49:48 +0100

    Add specific root command name

--- Sources/appstoreconnect/AppStoreConnect.swift
+++ Sources/appstoreconnect/AppStoreConnect.swift
@@ -3,6 +3,7 @@ import Foundation
 
 struct AppStoreConnect: ParsableCommand {
   static var configuration = CommandConfiguration(
+    commandName: "appstoreconnect",
     abstract: "An interface App Store Connect API",
     subcommands: [Profiles.self, Users.self]
   )
