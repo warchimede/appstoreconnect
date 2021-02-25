847b2f3db453125488712ed2275e2cf5d1792356
William Archimede <william.archimede@protonmail.com>
2021-02-25 20:07:22 +0100

    Download profiles and write them on disk in the right directory

--- Sources/appstoreconnect/AppStoreConnect+Profiles.swift
+++ Sources/appstoreconnect/AppStoreConnect+Profiles.swift
@@ -7,11 +7,39 @@ extension AppStoreConnect {
 
     @OptionGroup var options: AppStoreConnect.Options
 
+    private static func write(attributes: Profile.Attributes) throws {
+      guard
+        let directoryURL = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).first,
+        let fileData = Data(base64Encoded: attributes.profileContent)
+      else { throw AppStoreConnectError.cannotWriteProfile }
+
+      let fileName = attributes.name.replacingOccurrences(of: " ", with: "_")
+        .appending("_\(attributes.uuid).mobileprovision")
+      let fileURL = directoryURL.appendingPathComponent("MobileDevice")
+        .appendingPathComponent("Provisioning Profiles")
+        .appendingPathComponent(fileName)
+      try fileData.write(to: fileURL)
+    }
+
+    private static func handleResponse(data: Data) throws {
+      try JSONDecoder()
+        .decode(Profile.Response.self, from: data)
+        .data
+        .forEach {
+          try write(attributes: $0.attributes)
+        }
+    }
+
     mutating func run() {
       AppStoreConnect.fetch(endpoint: .profiles, options: options) { result in
         switch result {
         case .failure(let error): print("ERROR: \(error)")
-        case .success(let data): print(String(data: data, encoding: .utf8) ?? "Error: \(AppStoreConnectError.undecodableData)")
+        case .success(let data):
+          do {
+            try Self.handleResponse(data: data)
+          } catch {
+            print("ERROR: \(error)")
+          }
         }
       }
     }
