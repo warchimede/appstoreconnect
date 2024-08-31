6d635efd64939dad1c7b89350d9cddb0615a0fb6
William Archimede <1151996+warchimede@users.noreply.github.com>
2024-08-31 19:59:45 +0200

    refactor(Users): Conform to AsyncParsableCommand

--- Sources/appstoreconnect/AppStoreConnect+Users.swift
+++ Sources/appstoreconnect/AppStoreConnect+Users.swift
@@ -2,18 +2,20 @@ import ArgumentParser
 import Foundation
 
 extension AppStoreConnect {
-  struct Users: ParsableCommand {
+  struct Users: AsyncParsableCommand {
     static var configuration = CommandConfiguration(abstract: "Print the users")
 
     @OptionGroup var options: AppStoreConnect.Options
 
-    mutating func run() {
-      AppStoreConnect.fetch(endpoint: .users, options: options) { result in
-        switch result {
-        case .failure(let error): print("ERROR: \(error)")
-        case .success(let data): print(String(data: data, encoding: .utf8) ?? "Error: \(AppStoreConnectError.undecodableData)")
-        }
+    mutating func run() async throws {
+      let result = await AppStoreConnect.asyncFetch(endpoint: .users, options: options)
+
+      let output = switch result {
+      case .failure(let error): "ERROR: \(error)"
+      case .success(let data): String(data: data, encoding: .utf8) ?? "Error: \(AppStoreConnectError.undecodableData)"
       }
+
+      print(output)
     }
   }
 }
